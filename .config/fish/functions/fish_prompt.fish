ace88cd18f07370c424716e5a1eb586d67cad9d4
DustVoice <info@dustvoice.de>
2019-10-24 17:22:07 +0200

    Make column wrap and strip

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,9 +5,20 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_symbol '$'
     end
 
+    set -l prompt_prefix (set_color yellow)(whoami)'@'(set_color purple)(prompt_hostname)' '
+    set -l prompt_dir (set_color green)(prompt_pwd)
+
+    set -l column_aware_prompt (construct_column_aware_prompt $prompt_prefix $prompt_dir)
+
     if test -z $WINDOW
-        printf '%s%s@%s%s %s%s%s %s ' (set_color yellow) (whoami) (set_color purple) (prompt_hostname) (set_color green) (prompt_pwd) (set_color normal) $prompt_symbol
+        for dir in $column_aware_prompt
+            echo $dir
+        end
+
+        set_color normal
+
+        echo $prompt_symbol' '
     else
-        printf '%s%s@%s%s%s (%s)%s%s%s %s ' (set_color yellow) (whoami) (set_color purple) (prompt_hostname) (set_color white) (echo $WINDOW) (set_color green) (prompt_pwd) (set_color normal) $prompt_symbol
+        printf '%s%s@%s%s%s (%s)%s%s%s\n%s ' (set_color yellow) (whoami) (set_color purple) (prompt_hostname) (set_color white) (echo $WINDOW) (set_color green) (prompt_pwd) (set_color normal) $prompt_symbol
     end
 end
