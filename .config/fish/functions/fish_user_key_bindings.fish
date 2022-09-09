ea9869e367d9a3799f2668460277597a21f685fb
DustVoice <info@dustvoice.de>
2022-09-09 14:08:45 +0200

    Add sane keybindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,9 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
+
+    bind \f  -M insert 'accept-autosuggestion'
+    bind \b  -M insert 'cancel-commandline'
+    bind \v            'up_or_search'
+    bind \n            'down_or_search'
+    bind \cE -M insert 'edit_command_buffer'
 end
