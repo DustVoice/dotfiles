7ba4dc58a0406b7e6d6584f8eff6a0dad166b1bd
DustVoice <info@dustvoice.de>
2022-09-11 22:01:36 +0200

    Correct mistake in dmenu_fish and in user keybindings

--- .config/fish/functions/dmenu_fish.fish
+++ .config/fish/functions/dmenu_fish.fish
@@ -1,7 +1,7 @@
 function dmenu_fish --description 'Open fish agnostic dmenu'
     set fns "$(string replace -a ', ' \n $(functions))"
 
-    set commands $(string join \n $fns $(dmenu_path) | sort | dmenu -i -fn "$(get-font)" -nb "#282a36" -nf "#f8f8f2" -sb "#44475a" -sf "#f8f8f2")
+    set command_to_run $(string join \n $fns $(dmenu_path) | sort | dmenu -i -fn "$(get-font)" -nb "#282a36" -nf "#f8f8f2" -sb "#44475a" -sf "#f8f8f2")
 
     fish -c "$command_to_run" &
 end
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,10 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
 
+    bind \f            'accept-autosuggestion'
+    bind \b            'cancel-commandline'
     bind \f  -M insert 'accept-autosuggestion'
     bind \b  -M insert 'cancel-commandline'
-    bind \v            'up_or_search'
-    bind \n            'down_or_search'
+    bind \cE           'edit_command_buffer'
     bind \cE -M insert 'edit_command_buffer'
 end
