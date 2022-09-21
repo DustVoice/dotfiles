0dc52132d1e1c4a5e1a00a516291f20cc78ed2e0
DustVoice <info@dustvoice.de>
2022-09-21 10:16:04 +0200

    Adjust some configs and add doom emacs

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,19 +1,21 @@
-set -x LANG "en_US.UTF-8"
-set -x EDITOR "nvim"
-set -x TERMINAL "alacritty"
+set -gx LANG "en_US.UTF-8"
+set -gx EDITOR "nvim"
+set -gx TERMINAL "alacritty"
+set -gx GIT_PAGER "delta"
+set -gx DOOMPAGER "delta"
 
-set -x SSH_AUTH_SOCK "$(gpgconf --list-dir socketdir)/S.gpg-agent.ssh"
-set -x GPG_TTY $(tty)
+set -gx SSH_AUTH_SOCK "$(gpgconf --list-dir socketdir)/S.gpg-agent.ssh"
+set -gx GPG_TTY $(tty)
 
-set -x CXXFLAGS "-std=c++14"
+set -gx CXXFLAGS "-std=c++14"
 
-set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH
-set -x XDG_CONFIG_HOME "$HOME/.config"
+set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH
+set -gx XDG_CONFIG_HOME "$HOME/.config"
 
-set -x JUCE_PATH "$HOME/JUCE"
+set -gx JUCE_PATH "$HOME/JUCE"
 
-set -x LIBGL_ALWAYS_INDIRECT 1
+set -gx LIBGL_ALWAYS_INDIRECT 1
 
-set -x WINIT_X11_SCALE_FACTOR 1
+set -gx WINIT_X11_SCALE_FACTOR 1
 
-set -x MOZ_ENABLE_WAYLAND 1
+set -gx MOZ_ENABLE_WAYLAND 1
