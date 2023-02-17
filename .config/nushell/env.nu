5fdef5173dd1251f457266b0b98ccaecb8394a12
DustVoice <info@dustvoice.de>
2023-02-17 08:29:39 +0100

    Update nushell config, update doom package list

--- .config/nushell/env.nu
+++ .config/nushell/env.nu
@@ -2,28 +2,28 @@
 
 let-env STARSHIP_SHELL = "nu"
 
-def create_left_prompt [] {
-    starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
-}
-
-def create_right_prompt [] {
-    let time_segment = ([
-        (date now | date format '%m/%d/%Y %r')
-    ] | str join)
-
-    $time_segment
-}
+# def create_left_prompt [] {
+#     starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)' $'--keymap=($env.STARSHIP_KEYMAP)'
+# }
+#
+# def create_right_prompt [] {
+#     let time_segment = ([
+#         (date now | date format '%m/%d/%Y %r')
+#     ] | str join)
+#
+#     $time_segment
+# }
 
 # Use nushell functions to define your right and left prompt
-let-env PROMPT_COMMAND = {}
-let-env PROMPT_COMMAND_RIGHT = {}
+# let-env PROMPT_COMMAND = { create_left_prompt }
+# let-env PROMPT_COMMAND_RIGHT = { create_right_prompt }
 
 # The prompt indicators are environmental variables that represent
 # the state of the prompt
-let-env PROMPT_INDICATOR = { "" }
-let-env PROMPT_INDICATOR_VI_INSERT = { "> " }
-let-env PROMPT_INDICATOR_VI_NORMAL = { ": " }
-let-env PROMPT_MULTILINE_INDICATOR = { ">>> " }
+# let-env PROMPT_INDICATOR = { "" }
+# let-env PROMPT_INDICATOR_VI_INSERT = { "" }
+# let-env PROMPT_INDICATOR_VI_NORMAL = { "" }
+# let-env PROMPT_MULTILINE_INDICATOR = { "" }
 
 # Specifies how environment variables are:
 # - converted from a string to a value on Nushell startup (from_string)
--- .doom.d/config.el
+++ .doom.d/config.el
@@ -105,19 +105,52 @@
                  ("\\subsection{%s}" . "\\subsection*{%s}")
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
 
-(use-package! paw64-mode
-  :init
-  :config
-
-  (define-derived-mode paw64-mode
-    prog-mode
-    "Paw64"
-    "Major mode for 6502/6510 assembly with 64tass and/or paw64"
-    (set-syntax-table (make-syntax-table paw64-mode-syntax-table))
-    (set (make-local-variable 'font-lock-defaults) '(paw64-font-lock-keywords))
-    (set (make-local-variable 'indent-line-function) 'paw64-indent)
-
-    (add-hook 'post-command-hook #'paw64-post-command-hook nil t)
-    (electric-indent-local-mode -1)))
+;; (use-package! paw64-mode
+;;   :init
+;;   :config
+;; 
+;;   (define-derived-mode paw64-mode
+;;     prog-mode
+;;     "Paw64"
+;;     "Major mode for 6502/6510 assembly with 64tass and/or paw64"
+;;     (set-syntax-table (make-syntax-table paw64-mode-syntax-table))
+;;     (set (make-local-variable 'font-lock-defaults) '(paw64-font-lock-keywords))
+;;     (set (make-local-variable 'indent-line-function) 'paw64-indent)
+;; 
+;;     (add-hook 'post-command-hook #'paw64-post-command-hook nil t)
+;;     (electric-indent-local-mode -1)))
 
 (use-package! arm-mode :init :config)
+
+(use-package! ron-mode :init :config)
+
+(map! :map dap-mode-map
+      :leader
+      :prefix ("d" . "dap")
+      ;; basics
+      :desc "dap next"          "n" #'dap-next
+      :desc "dap step in"       "i" #'dap-step-in
+      :desc "dap step out"      "o" #'dap-step-out
+      :desc "dap continue"      "c" #'dap-continue
+      :desc "dap hydra"         "h" #'dap-hydra
+      :desc "dap debug restart" "r" #'dap-debug-restart
+      :desc "dap debug"         "s" #'dap-debug
+
+      ;; debug
+      :prefix ("dd" . "Debug")
+      :desc "dap debug recent"  "r" #'dap-debug-recent
+      :desc "dap debug last"    "l" #'dap-debug-last
+
+      ;; eval
+      :prefix ("de" . "Eval")
+      :desc "eval"                "e" #'dap-eval
+      :desc "eval region"         "r" #'dap-eval-region
+      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+      :desc "add expression"      "a" #'dap-ui-expressions-add
+      :desc "remove expression"   "d" #'dap-ui-expressions-remove
+
+      :prefix ("db" . "Breakpoint")
+      :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
