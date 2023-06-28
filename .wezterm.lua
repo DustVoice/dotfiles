ca6f0b67aa96b15d9804c168fbdf5dd5ff913fe6
DustVoice <info@dustvoice.de>
2023-06-28 11:36:55 +0200

    Only disable nushell shell_integration when wezterm is used (not fixed in current wezterm release)

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,6 +16,7 @@ config.default_prog = { 'nu' }
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = true
+
 config.window_decorations = "TITLE | RESIZE"
 
 return config
