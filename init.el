3e3984e7b1618caa1822f58dc3efa78fb36cf1af
DustVoice <info@dustvoice.de>
2024-10-10 21:20:13 +0200

    Doom - Enable debugger support

--- init.el
+++ init.el
@@ -89,7 +89,7 @@
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       (debugger +lsp)     ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
