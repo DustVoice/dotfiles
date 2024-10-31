f12ddc261fe4e1aef36824d3e48f7b9194401326
DustVoice <info@dustvoice.de>
2024-10-31 17:47:20 +0100

    Doom - Enable some org-mode options

--- init.el
+++ init.el
@@ -170,7 +170,11 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org
+         +hugo
+         +pomodoro
+         +present
+         +roam2)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
