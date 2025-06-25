2c52011ceefd97af18aa5c442da6473578b442b4
DustVoice <info@dustvoice.de>
2025-06-25 17:19:57 +0200

    Switch to fish

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,3 +1,16 @@
+# > Cursor
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block
+# < Cursor
+
+# > Bindings
+fish_vi_key_bindings --no-erase
+# < Bindings
+
 # > ENV
 set -gx LANG "en_US.UTF-8"
 set -gx EDITOR nvim
@@ -16,13 +29,6 @@ if command -q zoxide
     zoxide init fish | source
 end
 # < Zoxide
-
-# > Cursor
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
-# < Cursor
 
 # > Aliases
 alias cz="chezmoi"
