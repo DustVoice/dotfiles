d3620f377be3af1a0c0986fa1a4ee0157f162264
DustVoice <info@dustvoice.de>
2025-06-29 21:46:16 +0200

    Limit to one oh-my-posh theme, which I use from now on

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -23,7 +23,7 @@ set -gx TERMINAL ghostty
 
 # > Oh-My-Posh
 if command -q oh-my-posh
-    set -l config ~/.config/oh-my-posh/dustvoice-clean.omp.yaml
+    set -l config ~/.config/oh-my-posh/dustvoice.omp.yaml
     if test -e $config
         oh-my-posh init fish --config $config | source
     else
