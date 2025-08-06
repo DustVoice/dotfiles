5c430338c35dbd0b9765a664d576b303c930834a
DustVoice <info@dustvoice.de>
2025-08-06 14:09:47 +0200

    Add opam to fish

---
+++ home/dot_config/fish/conf.d/opam.fish
@@ -0,0 +1,10 @@
+if command -q opam
+    # BEGIN opam configuration
+    # 
+    # This is useful if you're using opam as it adds:
+    #   - the correct directories to the PATH
+    #   - auto-completion for the opam binary
+    # This section can be safely removed at any time if needed.
+    test -r '~/.opam/opam-init/init.fish' && source '~/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
+    # END opam configuration
+end
