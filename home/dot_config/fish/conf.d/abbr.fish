ee3f997c2275bcaa118113bfb0dde8678c97cf6c
DustVoice <info@dustvoice.de>
2025-08-18 09:49:24 +0200

    Disable using zellij by default

--- home/dot_config/fish/conf.d/abbr.fish
+++ home/dot_config/fish/conf.d/abbr.fish
@@ -1,5 +1,8 @@
 abbr -a -- sysu 'systemctl --user'
 abbr -a -- cz chezmoi
+abbr -a -c chezmoi -- a apply
+abbr -a -c chezmoi -- e edit
+abbr -a -c chezmoi -- u 'update -R'
 abbr -a -- ljj lazyjj
 abbr -a -- y yazi
 abbr -a -- activate 'source .venv/bin/activate.fish'
