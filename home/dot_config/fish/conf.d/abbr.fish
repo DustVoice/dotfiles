a477039fb3b3f8f5c9319e0f7ea4f95463def22e
DustVoice <info@dustvoice.de>
2025-08-18 17:57:54 +0200

    Correct wrong fish abbr

--- home/dot_config/fish/conf.d/abbr.fish
+++ home/dot_config/fish/conf.d/abbr.fish
@@ -3,7 +3,7 @@ abbr -a cz chezmoi
 abbr -a -c chezmoi a apply
 abbr -a -c chezmoi --set-cursor e 'edit % -a'
 abbr -a -c chezmoi g git
-abbr -a -c chezmoi u 'update -R'
+abbr -a -c chezmoi u 'update -aR'
 abbr -a ljj lazyjj
 abbr -a y yazi
 abbr -a activate 'source .venv/bin/activate.fish'
