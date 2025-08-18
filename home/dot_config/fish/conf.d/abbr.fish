59792fd5ff109de414c4c68d4e272005f88b670b
DustVoice <info@dustvoice.de>
2025-08-18 14:05:58 +0200

    Mod helix and fish abbr

--- home/dot_config/fish/conf.d/abbr.fish
+++ home/dot_config/fish/conf.d/abbr.fish
@@ -1,8 +1,9 @@
-abbr -a -- sysu 'systemctl --user'
-abbr -a -- cz chezmoi
-abbr -a -c chezmoi -- a apply
-abbr -a -c chezmoi -- e edit
-abbr -a -c chezmoi -- u 'update -R'
-abbr -a -- ljj lazyjj
-abbr -a -- y yazi
-abbr -a -- activate 'source .venv/bin/activate.fish'
+abbr -a sysu 'systemctl --user'
+abbr -a cz chezmoi
+abbr -a -c chezmoi a apply
+abbr -a -c chezmoi --set-cursor e 'edit % -a'
+abbr -a -c chezmoi g git
+abbr -a -c chezmoi u 'update -R'
+abbr -a ljj lazyjj
+abbr -a y yazi
+abbr -a activate 'source .venv/bin/activate.fish'
